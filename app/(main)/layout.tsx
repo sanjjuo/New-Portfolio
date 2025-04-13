c8da5fcd20e3e75837a2e0f00abf2cc90b97ee10
sanjjuo <sanjeedkoraliyil29399@gmail.com>
2025-04-13 17:02:25 +0400

    second commit

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -6,6 +6,7 @@ import Footer from "@/components/CommonUi/Footer/Footer";
 
 const poppins = Poppins({
   variable: "--font-poppins",
+  subsets: ["latin"],
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
 });
 
